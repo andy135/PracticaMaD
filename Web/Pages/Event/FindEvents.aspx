b92243fd57471b498311796188f27633edadbb3a
José Andy Quintero Melo <andy.qmelo@udc.es>
2015-11-30 17:47:52 +0100

    Página FindEvents implementada

---
+++ Web/Pages/Event/FindEvents.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/PracticaMaD.Master" AutoEventWireup="true" CodeBehind="FindEvents.aspx.cs" Inherits="Es.Udc.DotNet.PracticaMaD.Web.Pages.Event.FindEvents" meta:resourcekey="Page" %>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder_BodyContent"
+    runat="server">
+	<div id="form">
+        <form id="FindEventsForm" method="post" runat="server">
+			<div class="field">
+                <span class="label">
+                    <asp:Localize ID="lclKeywords" runat="server" meta:resourcekey="lclKeywords" /></span><span
+                        class="entry">
+                        <asp:TextBox CssClass="textbox" ID="txtKeys" runat="server" Width="100px" Columns="16" 
+                    meta:resourcekey="txtKeysResource1"></asp:TextBox>
+            </div>
+
+			<div class="field">
+                <span class="label">
+                    <asp:Localize ID="lclCategory" runat="server" meta:resourcekey="lclCategory" /></span><span
+                        class="entry">
+                        <asp:DropDownList ID="comboCategory" runat="server" Width="110px" 
+                    meta:resourcekey="comboCategoryResource1">
+                        </asp:DropDownList></span>
+            </div>
+
+            <div class="button">
+                <asp:Button CssClass="btn" ID="btnFindEvents" runat="server" OnClick="BtnFindEventsClick" meta:resourcekey="btnFindEvents" />
+            </div>
+        </form>
+    </div>
+</asp:Content>
---
+++ Web/Pages/Event/FindEvents.aspx.cs
@@ -0,0 +1,63 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model;
+using Es.Udc.DotNet.PracticaMaD.Model.EventService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Event
+{
+	public partial class FindEvents : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			List<Category> cats = GetCategories();
+			FillComboCategories(cats);
+		}
+		/*
+		private void UpdateComboCategory(String selectedCategory)
+		{
+			this.comboCategory.DataSource = Languages.GetLanguages(selectedCategory);
+			this.comboCategory.DataTextField = "text";
+			this.comboCategory.DataValueField = "value";
+			this.comboCategory.DataBind();
+			this.comboCategory.SelectedValue = selectedCategory;
+		}
+		*/
+
+		private void FillComboCategories(List<Category> cats)
+		{
+			this.comboCategory.DataTextField = "categoryName";
+			this.comboCategory.DataValueField = "categoryId";
+			this.comboCategory.DataSource = cats;
+			this.comboCategory.DataBind();
+		}
+
+		private List<Category> GetCategories()
+		{
+			IUnityContainer container = (IUnityContainer)HttpContext.Current.Application["unityContainer"];
+			IEventService eventService = container.Resolve<IEventService>();
+
+			return eventService.GetCategories();
+		}
+
+
+		protected void BtnFindEventsClick(object sender, EventArgs e)
+		{
+			string keys = this.txtKeys.Text;
+			string catId = this.comboCategory.SelectedValue;
+
+			/* Do action. */
+			String url =
+				String.Format("./FoundEvents.aspx?keys={0}" +
+					"&categoryId={1}", keys, catId);
+
+			Response.Redirect(Response.ApplyAppPathModifier(url));
+
+		}
+	}
+}
