49b101de48f2118ea8501337e6b771c07b9d50c5
Andy Quintero Melo <andy.qmelo@udc.es>
2016-01-17 20:04:19 +0100

    Iniciando Recomendations

---
+++ Web/Pages/Recomendation/Recomend.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Recomend.aspx.cs" Inherits="Es.Udc.DotNet.PracticaMaD.Web.Pages.Recomendation.Recomend" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder_BodyContent"
+    runat="server">
+	<div id="form">
+        <form id="RecomendForm" method="post" runat="server">
+			<div class="field">
+                <span class="label">
+                    <asp:Localize ID="lclRecomendation" runat="server" meta:resourcekey="lclComment" />
+                </span>
+                <span class="entry">
+                        <asp:TextBox CssClass="textbox" Columns="16" ID="txtRecomendation" runat="server" Width="200px" 
+                    meta:resourcekey="txtRecomendResource"></asp:TextBox>
+            </div>
+            <div class="field">
+                <asp:CheckBoxList ID="CheckBoxListGroups" runat="server" Width="200px">
+                </asp:CheckBoxList>
+            </div>
+            <div class="button">
+                <asp:Button CssClass="btn" ID="btnRecomend" runat="server" OnClick="BtnRecomendClick" meta:resourcekey="btnDoComment" />
+            </div>
+        </form>
+    </div>
+    </span>
+</asp:Content>
---
+++ Web/Pages/Recomendation/Recomend.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model.RecomendationService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using Es.Udc.DotNet.PracticaMaD.Web.Properties;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Recomendation
+{
+    public partial class Recomend : System.Web.UI.Page
+    {
+        long eventId,userId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            eventId = Convert.ToInt64(Request.Params.Get("eventId"));
+
+            if (!SessionManager.IsUserAuthenticated(Context))
+            {
+                /* Do action. */
+                String url =
+                    Settings.Default.PracticaMaD_applicationURL +
+                                    "Pages/User/Authentication.aspx" + "?eventId=" + eventId;
+
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+            else
+            {
+                userId = SessionManager.GetUserSession(Context).UserProfileId;
+            }
+        }
+
+
+        protected void BtnDoCommentClick(object sender, EventArgs e)
+        {
+            string recomendation = this.txtRecomendation.Text;
+
+            IUnityContainer container = (IUnityContainer)HttpContext.Current.Application["unityContainer"];
+            IRecomendationService recomendService = container.Resolve<IRecomendationService>();
+
+            /* Do action. */
+            String url =
+                Settings.Default.PracticaMaD_applicationURL +
+                                "Pages/MainPage.aspx";
+
+            Response.Redirect(Response.ApplyAppPathModifier(url));
+
+        }
+    }
+}
