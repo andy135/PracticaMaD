09d45b4d55e0dd212d63db6b694d76e9d5c8f58a
Iago <Santos Domnguez>
2016-01-18 16:03:11 +0100

    Preparando el modelo para modificar comentarios

---
+++ Web/Pages/Comment/ModifyComment.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ModifyComment.aspx.cs" Inherits="Es.Udc.DotNet.PracticaMaD.Web.Pages.Comment.ModifyComment" meta:resourcekey="Page" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder_BodyContent"
+    runat="server">
+	<div id="form">
+        <form id="ModifyCommentForm" method="post" runat="server">
+			<div class="field">
+                <span class="label">
+                    <asp:Localize ID="lclComment" runat="server" meta:resourcekey="lclComment" />
+                </span>
+                <span class="entry">
+                        <asp:TextBox CssClass="textbox" TextMode="multiline" Columns="16" ID="txtComment" runat="server" Width="200px" 
+                    meta:resourcekey="txtCommentResource"></asp:TextBox>
+            </div>
+            <div class="field">
+                <span class="label">
+                    <asp:Localize ID="lclTags" runat="server" meta:resourcekey="lclTags" />
+                </span>
+                <span class="entry">
+                        <asp:TextBox CssClass="textbox" TextMode="multiline" Columns="16" ID="txtTags" runat="server" Width="200px" 
+                    meta:resourcekey="txtTagResource"></asp:TextBox>
+            </div>
+
+            <div class="button">
+                <asp:Button CssClass="btn" ID="btnDoComment" runat="server" OnClick="BtnDoCommentClick" meta:resourcekey="btnDoComment" />
+            </div>
+        </form>
+    </div>
+</asp:Content>
---
+++ Web/Pages/Comment/ModifyComment.aspx.cs
@@ -0,0 +1,75 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model;
+using Es.Udc.DotNet.PracticaMaD.Model.CommentDao;
+using Es.Udc.DotNet.PracticaMaD.Model.CommentService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+using Es.Udc.DotNet.PracticaMaD.Web.Properties;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Comment
+{
+    public partial class ModifyComment : System.Web.UI.Page
+    {
+        long eventId,userId,commentId;
+        ICommentService commentService;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            eventId = Convert.ToInt64(Request.Params.Get("eventId"));
+            commentId = Convert.ToInt64(Request.Params.Get("commentId"));
+
+            IUnityContainer container = (IUnityContainer)HttpContext.Current.Application["unityContainer"];
+            commentService = container.Resolve<ICommentService>();
+
+            CommentInfo c = commentService.GetCommentById(commentId);
+
+            this.txtComment.Text = c.texto;
+
+            if (!SessionManager.IsUserAuthenticated(Context))
+            {
+                /* Do action. */
+                String url =
+                    Settings.Default.PracticaMaD_applicationURL +
+                                    "Pages/User/Authentication.aspx" + "?eventId=" + eventId;
+
+                Response.Redirect(Response.ApplyAppPathModifier(url));
+            }
+            else
+            {
+                userId = SessionManager.GetUserSession(Context).UserProfileId;
+            }
+        }
+
+
+        protected void BtnDoCommentClick(object sender, EventArgs e)
+        {
+            string comment = this.txtComment.Text;
+            string tags = this.txtTags.Text;
+
+            IUnityContainer container = (IUnityContainer)HttpContext.Current.Application["unityContainer"];
+            ICommentService commentService = container.Resolve<ICommentService>();
+
+            //Parsea los tags y elimina los espacios sobrantes
+            List<String> t = tags.Split('#').ToList();
+            foreach(String s in t)
+            {
+                s.Trim();
+            }
+
+            commentService.DoCommentWithTags(userId,eventId,comment, t);
+
+            /* Do action. */
+            String url =
+                Settings.Default.PracticaMaD_applicationURL +
+                                "Pages/MainPage.aspx";
+
+            Response.Redirect(Response.ApplyAppPathModifier(url));
+
+        }
+    }
+}
